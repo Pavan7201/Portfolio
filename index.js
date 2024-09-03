cb84999d506412df8db76ed468b3a09c0e2b44a3
Pavan Kumar Sahu <pavankumarsahu2000@gmail.com>
2024-09-03 13:54:29 -0400

    Updated <a>, <button>, and back to top button

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('header');
 
     function handleScroll() {
-        if (window.scrollY > 750) { // Scroll position to apply gray background
-            // body.classList.add('background-gray');
+        if (window.scrollY > 750) {
+            body.classList.add('background-gray');
             body.classList.remove('background-dark');
             header.classList.remove('background-dark');
-        } else if (window.scrollY > 400) { // Scroll position to apply dark background
+        } else if (window.scrollY > 400) {
             body.classList.add('background-dark');
             body.classList.remove('background-gray');
             header.classList.add('background-dark');
@@ -20,3 +20,31 @@ document.addEventListener('DOMContentLoaded', function () {
 
     window.addEventListener('scroll', handleScroll);
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.header-right a').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault(); 
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+});
+
+function logo() {
+    window.location.reload();
+}
+
+function GoToContact() {
+    const contentElement = document.getElementById('contact');
+    contentElement.scrollIntoView({behavior: 'smooth'});
+}
+
+function scrollToContent() {
+    const contentElement = document.getElementById('body');
+    contentElement.scrollIntoView({ behavior: 'smooth' });
+}
